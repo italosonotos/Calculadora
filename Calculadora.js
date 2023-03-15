3b4fc885279ac63f13f963b95795eeb212589077
italosonotos <107754956+italosonotos@users.noreply.github.com>
2023-03-14 22:50:56 -0300

    Finish

--- Calculadora.js
+++ Calculadora.js
@@ -9,22 +9,22 @@ let operador;
 let numeroAnterior;
 
 const operacaoPendente = () => operador !== undefined;
+
 const calcular = () =>{
     if(operacaoPendente()){
-        const numeroAtual = parseFloat(display.textContent);
+        const numeroAtual = parseFloat(display.textContent.replace(',','.'));
         novoNumero = true
-        if(operador == '+'){
-            atualizarDisplay(numeroAnterior + numeroAtual);
-        }
+        const resultado = eval (`${numeroAnterior}${operador}${numeroAtual}`);
+        atualizarDisplay(resultado);
     }
 }
 
 const atualizarDisplay = (texto) =>{
     if (novoNumero){
-        display.textContent = texto; 
+        display.textContent = texto.toLocaleString('BR'); 
         novoNumero = false
     }else{ 
-        display.textContent += texto;
+        display.textContent += texto.toLocaleString('BR');
     }
     
 }
@@ -38,7 +38,48 @@ const selecionarOperador = (evento) => {
         calcular()
         novoNumero = true;
         operador = evento.target.textContent;
-        numeroAnterior = parseFloat(display.textContent);
+        numeroAnterior = parseFloat(display.textContent.replace(',','.'));
     }
 }
-operadores.forEach (operador => operador.addEventListener('click',selecionarOperador))
+operadores.forEach (operador => operador.addEventListener('click',selecionarOperador));
+
+const acionarigual = () => {
+    calcular();
+    operador = undefined;
+}
+
+document.getElementById('igual').addEventListener('click', acionarigual);
+
+
+const limparDisplay = () => display.textContent = '';
+document.getElementById('limparDisplay').addEventListener('click', limparDisplay);
+
+const limparCalculo = () =>{
+    limparDisplay();
+    operador = undefined;
+    novoNumero = true;
+    numeroAnterior = undefined;
+}
+document.getElementById('limparCalculo').addEventListener('click', limparCalculo);
+
+const removerUltimoNumero = () => display.textContent = display.textContent.slice(0, -1);
+document.getElementById('backspace').addEventListener('click', removerUltimoNumero);
+
+const inverterSinal = () => {
+    novoNumero = true;
+    atualizarDisplay(display.textContent * -1);
+} 
+document.getElementById('inverter').addEventListener('click', inverterSinal);
+
+const existedDcimal = () => display.textContent.indexOf(',') != -1;
+const existeValor = () => display.lenght > 0 ;
+const inserirDecimal = () => {
+    if(!existedDcimal()){
+        if(existeValor()){
+            atualizarDisplay(',');
+        }else{
+            atualizarDisplay(',');
+        }
+    }
+} 
+document.getElementById('decimal').addEventListener('click', inserirDecimal);
